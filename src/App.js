999063b7d5f2834c0e36a48b0669316ccb8bab38
anirudhreddyrachamalla <anirudhrachamalla@gmail.com>
2023-04-03 12:11:17 +0530

    create group

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+const { ethers } = require("ethers");
 
 function App() {
+  async function loginCreateGroup() {
+    const semaphoreAbi = ["function createGroup(uint256,uint256,address)"];
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    let accounts = await provider.send("eth_requestAccounts", []);
+    console.log(accounts);
+    const signer = provider.getSigner();
+    console.log(signer);
+    let contractAddress = "0x220fBdB6F996827b1Cf12f0C181E8d5e6de3a36F";
+    const semaphore = new ethers.Contract(
+      contractAddress,
+      semaphoreAbi,
+      provider
+    );
+    const groupId = 25646546560;
+    const admin = "0xCAFd22C8e7aD60e73cfB0bae740167C4eB1Bd62B";
+    const semaphoreWithSigner = semaphore.connect(signer);
+    const tx = semaphoreWithSigner.createGroup(
+      groupId,
+      20,
+      admin
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +33,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          sx={{ color: "#fff", textTransform: "none" }}
+          onClick={loginCreateGroup}
         >
-          Learn React
-        </a>
+          {" "}
+          Connect Wallet
+        </button>
       </header>
     </div>
   );
